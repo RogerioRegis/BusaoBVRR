cbc94a3268ef2318dc7e4809edef8153f1a79e1b
Rogerio Regis <rog_reg@hotmail.com>
2014-11-02 01:07:53 -0400

    [ADDJUST] configuração do autoload
    Signed-off-by: Rogerio Regis <rog_reg@hotmail.com>

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -3,10 +3,10 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn'    => 'mysql:dbname=zf2_livraria;host=localhost',
-        'driver_options' => array(
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        )
+        'dsn'    => 'mysql:dbname=busao;host=localhost',
+//        'driver_options' => array(
+//            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+//        )
     ),
     'service_manager' => array(
         'factories' => array(
